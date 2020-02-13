7e834fa99714f622b2afa99c6b6fd426efae814e
steven_jiangxf <steven_jiangxf@hotmail.com>
2020-02-13 19:47:46 +0800

    update README.md

--- taskflow/task_receiver.py
+++ taskflow/task_receiver.py
@@ -21,7 +21,7 @@ def message_process(flow_instance_id):
         # get instance id
         # 获取需要运行的模块
         filename = os.path.join(base_dir, 'task_run.py')
-        output_filename = "/var/log/taskflows/task_run_%s.log" % flow_instance_id
+        output_filename = "/var/log/taskflow/task_run_%s.log" % flow_instance_id
         with open(output_filename, "a") as outfile:
             subprocess.Popen(["python3", "-u", filename, "-i", flow_instance_id], stdout=outfile, stderr=subprocess.STDOUT)
         #save subprocessid,flow_instance_id,worker_host_name to redis
