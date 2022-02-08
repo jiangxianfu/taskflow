b887e3e1ca7155166b6663f4ad809fcc3cb09c83
jiangxianfu <steven_jiangxf@hotmail.com>
2022-02-08 16:58:51 +0800

    update

--- tests/test_redisdb.py
+++ tests/test_redisdb.py
@@ -24,4 +24,4 @@ class TestRedisDB(unittest.TestCase):
         time.sleep(10)
         data = self.db.pop_check_queue(10)
         assert len(data) > 0
-        print(type(data[0]), data[0])
+        print('test check queue data:',type(data[0]), data[0])
