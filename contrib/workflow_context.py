34ab6e10f41eb0cc4ff5a0db573d64be15e02604
jiangxianfu <steven_jiangxf@hotmail.com>
2022-02-10 11:59:08 +0800

    fix: update name

--- contrib/workflow_context.py
+++ contrib/workflow_context.py
@@ -7,13 +7,13 @@ class WorkflowContext(object):
     def __init__(self, db: TaskFlowDB, instance_id: int, parent_id: int):
         self.instance_id = instance_id
         self.parent_id = parent_id
-        self.taskflowdb = db
+        self.db = db
 
     def get_root_argument(self):
         if self.parent_id > 0:
-            data = self.taskflowdb.get_instance_json(True, instance_id=self.parent_id)
+            data = self.db.get_instance_json(True, instance_id=self.parent_id)
         else:
-            data = self.taskflowdb.get_instance_json(True, instance_id=self.instance_id)
+            data = self.db.get_instance_json(True, instance_id=self.instance_id)
         if data:
             data = json.loads(data)
             return data
@@ -21,9 +21,9 @@ class WorkflowContext(object):
 
     def get_step_argument(self, step_name):
         if self.parent_id > 0:
-            data = self.taskflowdb.get_instance_json(True, parent_id=self.parent_id, name=step_name)
+            data = self.db.get_instance_json(True, parent_id=self.parent_id, name=step_name)
         else:
-            data = self.taskflowdb.get_instance_json(True, instance_id=self.instance_id, name=step_name)
+            data = self.db.get_instance_json(True, instance_id=self.instance_id, name=step_name)
         if data:
             data = json.loads(data)
             return data
@@ -31,9 +31,9 @@ class WorkflowContext(object):
 
     def get_step_result(self, step_name):
         if self.parent_id > 0:
-            data = self.taskflowdb.get_instance_json(False, parent_id=self.parent_id, name=step_name)
+            data = self.db.get_instance_json(False, parent_id=self.parent_id, name=step_name)
         else:
-            data = self.taskflowdb.get_instance_json(False, instance_id=self.instance_id, name=step_name)
+            data = self.db.get_instance_json(False, instance_id=self.instance_id, name=step_name)
         if data:
             data = json.loads(data)
             return data
