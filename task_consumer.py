829ed24328dcfc31f266e752cee3fce96b373c12
steven <steven_jiangxf@hotmail.com>
2023-09-27 21:41:11 +0800

    fix: update

--- task_consumer.py
+++ task_consumer.py
@@ -14,22 +14,38 @@ import traceback
 import sys
 import os
 import socket
+import psutil
+import subprocess
 from core import settings
 from core.redisdb import RedisDB
 from core.taskflowdb import TaskFlowDB
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
+task_run_file = "%s/task_run.py" % os.path.dirname(os.path.abspath(__file__))
+
+
+def check_run():
+    memory = psutil.virtual_memory()
+    if memory.percent > 80:
+        logging.warning("waiting, memory usage: %s" % repr(memory))
+        time.sleep(30)
+        return False
+    _, data = subprocess.getstatusoutput("ps -ef|grep '%s -i '|wc -l" % task_run_file)
+    process_list_count = int(data.strip()) - 2
+    if process_list_count > 100:
+        logging.warning("waiting, process count: %s, can't do task." % process_list_count)
+        time.sleep(30)
+        return False
+    logging.info('process count:%s,can do task.', process_list_count)
+    return True
 
 
 def message_process(instance_id):
     try:
         # 获取需要运行的模块
-        task_run_file = "task_run.py"
         output_filename = settings.TASK_RUN_LOG_FORMAT % instance_id
-        logging.debug("output_filename:%s", output_filename)
-        logging.debug("task_run_filename:%s", task_run_file)
-        logging.debug("task python bin location:%s", settings.PYTHONBIN)
         os.system("nohup %s -u %s %s >%s 2>&1 &" % (settings.PYTHONBIN, task_run_file, instance_id, output_filename))
+        logging.info("task_run instance:%s" % instance_id)
         taskflowdb = TaskFlowDB()
         worker_hostname = socket.gethostname()
         taskflowdb.save_instance_status(instance_id, 'running', worker_hostname=worker_hostname)
@@ -42,10 +58,11 @@ def main():
     # get redis data
     redisdb = RedisDB()
     while True:
-        data = redisdb.pop_run_queue()
-        if data is not None:
-            message_process(int(data))
-        time.sleep(1)
+        if check_run():
+            data = redisdb.pop_run_queue()
+            if data is not None:
+                message_process(int(data))
+            time.sleep(1)
 
 
 if __name__ == '__main__':
